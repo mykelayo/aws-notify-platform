12d81e2e26187ea20baf28395d04848e05f9f0bb
Michael Bello <michaelayo360@gmail.com>
2026-05-05 15:03:54 +0000

    feat(dev): expose messaging outputs (sns_topic_arn, sqs_queue_url, dlq_url)

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -9,3 +9,15 @@ output "private_subnet_ids" {
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
 }
+
+output "sns_topic_arn" {
+  value = module.messaging.sns_topic_arn
+}
+
+output "sqs_queue_url" {
+  value = module.messaging.sqs_queue_url
+}
+
+output "dlq_url" {
+  value = module.messaging.dlq_url
+}
