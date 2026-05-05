adf87518bbc4c087135662b94b23c7de46a23682
Michael Bello <michaelayo360@gmail.com>
2026-05-05 15:24:54 +0000

    feat(dev): integrate compute module (Lambda processor)

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = "~> 1.15.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.43.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "vpc" {
   source = "../../modules/vpc"
 
@@ -28,9 +13,28 @@ module "messaging" {
   source = "../../modules/messaging"
 
   name_prefix        = var.name_prefix
-  max_receive_count  = 3
+  max_receive_count  = var.max_receive_count
   create_dlq_alarm   = true
-  alarm_actions      = []  # TODO: add SNS topic ARN for email (Phase 4)
+  alarm_actions      = []  # TODO: add SNS topic ARN for email
   ok_actions         = []
   tags               = var.tags
+}
+
+module "compute" {
+  source = "../../modules/compute"
+
+  name_prefix           = var.name_prefix
+  lambda_zip_path       = var.lambda_zip_path
+  lambda_timeout_seconds = var.lambda_timeout_seconds
+  lambda_memory_mb      = var.lambda_memory_mb
+
+  enable_vpc        = true
+  private_subnet_ids = module.vpc.private_subnet_ids
+  # For now, no security group for Lambda (will create one later for Aurora)
+  security_group_ids = [module.vpc.default_security_group_id]
+  
+  sqs_queue_arn      = module.messaging.sqs_queue_arn
+  sqs_batch_size     = 1
+
+  tags = var.tags
 }
