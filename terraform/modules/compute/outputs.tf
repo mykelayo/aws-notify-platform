f58e13fb5a6e23d94ac2c20c0d1de2073694b401
Michael Bello <michaelayo360@gmail.com>
2026-05-05 15:24:53 +0000

    feat(compute): add outputs.tf – Lambda ARN, name, role ARN

---
+++ terraform/modules/compute/outputs.tf
@@ -0,0 +1,14 @@
+output "lambda_function_arn" {
+  description = "ARN of the message processor Lambda"
+  value       = aws_lambda_function.message_processor.arn
+}
+
+output "lambda_function_name" {
+  description = "Name of the message processor Lambda"
+  value       = aws_lambda_function.message_processor.function_name
+}
+
+output "lambda_role_arn" {
+  description = "IAM role ARN used by Lambda"
+  value       = aws_iam_role.lambda_execution.arn
+}
