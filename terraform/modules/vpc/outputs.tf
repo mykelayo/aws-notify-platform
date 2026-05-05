729138c7aae4924c3ce424ec00cfc5cbc478aa19
Michael Bello <michaelayo360@gmail.com>
2026-05-05 12:45:14 +0000

    feat(vpc): add outputs.tf exposing vpc_id, subnet lists, nat_gateway_id\

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_id" {
+  description = "NAT Gateway ID (single in AZ-a)"
+  value       = aws_nat_gateway.this.id
+}
+
+output "availability_zones" {
+  description = "Availability zones used"
+  value       = var.availability_zones
+}
