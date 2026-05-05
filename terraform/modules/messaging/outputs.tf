a7a901c5eb8dc34b2e7c66f06813c3ef32000f5c
Michael Bello <michaelayo360@gmail.com>
2026-05-05 15:03:53 +0000

    feat(messaging): add outputs.tf – sns_topic_arn, sqs_queue_arn, dlq_arn, queue URLs

---
+++ terraform/modules/messaging/outputs.tf
@@ -0,0 +1,24 @@
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic"
+  value       = aws_sns_topic.notifications.arn
+}
+
+output "sqs_queue_arn" {
+  description = "ARN of the main SQS queue"
+  value       = aws_sqs_queue.main.arn
+}
+
+output "sqs_queue_url" {
+  description = "URL of the main SQS queue"
+  value       = aws_sqs_queue.main.id
+}
+
+output "dlq_arn" {
+  description = "ARN of the DLQ"
+  value       = aws_sqs_queue.dlq.arn
+}
+
+output "dlq_url" {
+  description = "URL of the DLQ"
+  value       = aws_sqs_queue.dlq.id
+}
